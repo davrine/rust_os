af47653a941cb3b1ba6d33a14f05017204d4cebc
David <davrine.dl@gmail.com>
2022-10-29 00:35:12 -0400

    disabled unwinding by changing the panic strategy in cargo.toml and set up skeleton of panic_handler in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // Set rust-analyzer.checkOnSave.allTargets to false
+#![no_main] // disbales rusts default entry point
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
